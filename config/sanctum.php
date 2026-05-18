5728f66ef9fe4acb11622caef7d34ed848d42dda
Tu Nombre Real <tu.email@ejemplo.com>
2026-05-18 18:12:22 -0400

    security: Sanctum token expiration 120min, prune schedule, expired/idle session messages (FASE 2)

--- config/sanctum.php
+++ config/sanctum.php
@@ -9,7 +9,7 @@ return [
 
     'guard' => ['web'],
 
-    'expiration' => null,
+    'expiration' => env('SANCTUM_TOKEN_EXPIRATION', 120),
 
     'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
 
